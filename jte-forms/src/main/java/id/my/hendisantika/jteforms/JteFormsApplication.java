3f93bd6b6eab45173982af8de1571b0200a1c5cd
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 07:27:35 +0700

    feat: Add jte-forms🫸🌀✏️📗🐧🐳

---
+++ jte-forms/src/main/java/id/my/hendisantika/jteforms/JteFormsApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jteforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JteFormsApplication.class, args);
+    }
+
+}
---
+++ jte-forms/src/test/java/id/my/hendisantika/jteforms/JteFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jteforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JteFormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
