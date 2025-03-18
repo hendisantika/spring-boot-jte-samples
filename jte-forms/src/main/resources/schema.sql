25aeef9bda41492eacbb73c17675ba2dcc7434ec
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 07:29:55 +0700

    feat: Add schema.sql file🫸🌀✏️📗🐧🐳

---
+++ jte-forms/src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE if NOT EXISTS USERS
+(
+    id
+    SERIAL
+    PRIMARY
+    KEY,
+    first_name
+    VARCHAR
+(
+    255
+),
+    last_name VARCHAR
+(
+    255
+),
+    email VARCHAR
+(
+    255
+),
+    country VARCHAR
+(
+    255
+),
+    street_address VARCHAR
+(
+    255
+),
+    city VARCHAR
+(
+    255
+),
+    region VARCHAR
+(
+    255
+),
+    postal_code VARCHAR
+(
+    255
+)
+    );
