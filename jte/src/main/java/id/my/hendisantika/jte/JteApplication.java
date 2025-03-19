70624e17986c542649d3593ac92822440a3c8b78
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 07:14:08 +0700

    feat: Add jte module🫸🌀✏️📗🐧🐳

---
+++ jte/src/main/java/id/my/hendisantika/jte/JteApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JteApplication.class, args);
+    }
+
+}
---
+++ jte/src/test/java/id/my/hendisantika/jte/JteApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
