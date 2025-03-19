8ad969b1f046444e0e92a165733cc39beb955f75
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 07:16:45 +0700

    feat: Add Page Record🫸🌀✏️📗🐧🐳

---
+++ jte/src/main/java/id/my/hendisantika/jte/Page.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.jte;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jte-samples
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/03/25
+ * Time: 07.16
+ * To change this template use File | Settings | File Templates.
+ */
+public record Page(String title, String description) {
+}
